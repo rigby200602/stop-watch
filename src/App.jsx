669448071dbac011c4dfab9cb51e16eaeabed93f
rigby200602 <tungle3456@gmail.com>
2025-09-03 22:43:40 +0700

    adjust width

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex justify-center bg-white'>
+    <div className='flex justify-center bg-white m-auto w-[50%]'>
       a
     </div>
   )
