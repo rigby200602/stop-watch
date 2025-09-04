35b4a45ed41d3255079b40f23cf6e8d6ff116703
rigby200602 <tungle3456@gmail.com>
2025-09-04 23:06:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ function App() {
   return (
     <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl'>
       <h1 className='text-2xl text-center text-black font-bold pt-16'>Stop Watch</h1>
-      <div className='text-center text-black text-6xl font-mono pt-16'>00:00:00</div>
-      </div>
+      
     </div>
   )
 }
