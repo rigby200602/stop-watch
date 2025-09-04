54d94fa2dcd009be7ce1746273c6b0b63ec901ad
rigby200602 <tungle3456@gmail.com>
2025-09-04 23:06:02 +0700

    center text

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import './App.css'
 function App() {
 
   return (
-    <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl mt-26 shadow-2xl'>
-      <h1 className='text-2xl text-center text-black my-auto'>Stop Watch</h1>
+    <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl'>
+      <h1 className='text-2xl text-center text-black font-bold pt-16'>Stop Watch</h1>
+      <div className='text-center text-black text-6xl font-mono pt-16'>00:00:00</div>
+      </div>
     </div>
   )
 }
