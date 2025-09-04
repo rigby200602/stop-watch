a10ce078e5ecb350509f27bfbe2a15f566211c7b
rigby200602 <tungle3456@gmail.com>
2025-09-04 23:08:02 +0700

    add timer

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl text-black'>
-      <h1 className='text-2xl text-center  font-bold pt-16'>Stop Watch</h1>
-      <div className='text-center'>
-
+      <h1 className='text-2xl text-center font-bold py-16'>Stop Watch</h1>
+      <div className='text-center font-extrabold py-auto text-4xl'>
+        00:00
       </div>
     </div>
   )
