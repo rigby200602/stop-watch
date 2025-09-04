72da4478476ff3c0dd576ca370fb231ef6910864
rigby200602 <tungle3456@gmail.com>
2025-09-04 23:31:22 +0700

    fix padding h1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl text-black'>
-      <h1 className='text-4xl text-center font-bold py-24'>Stop Watch</h1>
+      <h1 className='text-4xl text-center font-bold pt-24 pb-20'>Stop Watch</h1>
       <div className='text-center font-extrabold text-4xl'>
         00:00
       </div>
