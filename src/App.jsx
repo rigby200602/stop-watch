532ac6c8f8d24215157a3e83cbd65ce2af9dc6a0
rigby200602 <tungle3456@gmail.com>
2025-09-04 23:19:24 +0700

    add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,20 @@ function App() {
 
   return (
     <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl text-black'>
-      <h1 className='text-2xl text-center font-bold py-16'>Stop Watch</h1>
-      <div className='text-center font-extrabold py-auto text-4xl'>
+      <h1 className='text-4xl text-center font-bold py-24'>Stop Watch</h1>
+      <div className='text-center font-extrabold text-4xl'>
         00:00
+      </div>
+      <div className='flex justify-center gap-14 my-16'>
+        <button>
+          Start
+        </button>
+        <button>
+          Continue
+        </button>
+        <button>
+          Stop
+        </button>
       </div>
     </div>
   )
