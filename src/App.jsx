df4ff728cc8e181466d997c9d589739457d6cdfa
rigby200602 <tungle3456@gmail.com>
2025-09-05 20:31:29 +0700

    dynamic timer

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import './App.css'
 function App() {
   const [showSeconds, setShowSeconds] = useState(0)
   const [showMinutes, setShowMinutes] = useState(0)
+  if (showSeconds === 0) {
+    setShowSeconds('00')
+  } 
+  if (showMinutes === 0) {
+    setShowMinutes('00')
+  }
   return (
     <div className= 'justify-center bg-white m-auto w-[50%] min-h-128 rounded-4xl my-26 shadow-2xl text-black'>
       <h1 className='text-4xl text-center font-bold pt-24 pb-20'>Stop Watch</h1>
       <div className='text-center font-extrabold text-4xl'>
-        00:00
+        {showMinutes}:{showSeconds}
       </div>
       <div className='flex justify-center gap-4 my-16'>
         <button className='bg-indigo-600 text-white font-bold px-auto py-4 rounded-2xl w-40 cursor-pointer hover:bg-indigo-800'>
